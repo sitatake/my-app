05a02002d2d448a9bc39fc98bdc18e604f955b3d
takeshita <tkstdic@gmail.com>
2024-03-17 21:30:06 +0900

    feat: add feature to add and edit, delete task

--- frontend/app/components/TodoList.tsx
+++ frontend/app/components/TodoList.tsx
@@ -1,15 +1,84 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 
-const TodoList = () => {
+export type todotype = {
+  id: number;
+  text: string;
+  finish: boolean;
+};
+
+export type Props = {
+  todolist: todotype[];
+  setTodos: Dispatch<SetStateAction<todotype[]>>;
+};
+
+const TodoList = ({ todolist, setTodos }: Props) => {
+  const [editingId, setEditingId] = React.useState<number | null>(null);
+  const [editText, setEditText] = React.useState<string>("");
+
+  const handleFinish = (id: number) => {
+    const finishedtodo = todolist.find((_todo) => _todo.id === id);
+    if (finishedtodo) {
+      finishedtodo.finish = true;
+      const newtodolist = todolist.filter((_todo) => _todo.id !== id);
+      setTodos([...newtodolist, finishedtodo]);
+    }
+  };
+
+  const handleEdit = (id: number, text: string) => {
+    if (editingId === id) {
+      const newTodos = todolist.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, text: editText };
+        }
+        return todo;
+      });
+      setTodos(newTodos);
+      setEditingId(null);
+      setEditText("");
+    } else {
+      setEditingId(id);
+      setEditText(text);
+    }
+  };
+
   return (
     <ul className="space-y-3">
-      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow-md">
-        <span>散歩</span>
-        <div>
-          <button className="text-green-500 mr-3">edit</button>
-          <button className="text-red-500 ">Delete</button>
-        </div>
-      </li>
+      {todolist.map(
+        (todo, index) => (
+          !todo.finish && <li
+            key={index}
+            className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow-md"
+          >
+            {editingId === todo.id ? (
+              <input
+                type="text"
+                value={editText}
+                onChange={(e) => setEditText(e.target.value)}
+                className="border-2"
+              />
+            ) : (
+              <span>
+                {todo.text}
+                {todo.id}
+              </span>
+            )}
+            <div>
+              <button
+                className="text-green-500 mr-3"
+                onClick={() => handleEdit(todo.id, todo.text)}
+              >
+                {editingId === todo.id ? "Save" : "Edit"}
+              </button>
+              <button
+                className="text-red-500 "
+                onClick={() => handleFinish(todo.id)}
+              >
+                Finish!
+              </button>
+            </div>
+          </li>
+        )
+      )}
     </ul>
   );
 };
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,30 @@
-import Image from "next/image";
+"use client";
+
 import AddTask from "./components/AddTask";
-import TodoList from "./components/TodoList";
+import TodoList, { todotype } from "./components/TodoList";
+import { useState } from "react";
+
+const todosDefault: todotype[] = [
+  {
+    id: 1,
+    text: "プログラミング",
+    finish: false,
+  },
+  {
+    id: 2,
+    text: "プログラミング",
+    finish: false,
+  },
+  {
+    id: 3,
+    text: "プログラミング",
+    finish: false,
+  },
+];
 
 export default function Home() {
+  const [todos, setTodos] = useState<todotype[]>(todosDefault);
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
       <h1 className="text-4xl font-bold text-gray-700 -mt-32">
@@ -10,8 +32,8 @@ export default function Home() {
       </h1>
       <div className="w-full max-w-xl mt-5">
         <div className="w-full px-8 py-6 bg-white shadow-lg rounded-lg">
-          <AddTask />
-          <TodoList />
+          <AddTask todolist={todos} setTodos={setTodos} />
+          <TodoList todolist={todos} setTodos={setTodos} />
         </div>
       </div>
     </main>
